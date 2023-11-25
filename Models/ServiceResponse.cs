9b37eceae79045ea41f7e0f1661634da5f5408f4
TPGPL <TPGPL@users.noreply.github.com>
2023-11-26 00:50:04 +0100

    Add project files.

---
+++ Models/ServiceResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PamiwMauiApp.Models;
+
+public class ServiceResponse<T>
+{
+    [JsonPropertyName("data")]
+    public T? Data { get; set; }
+    [JsonPropertyName("wasSuccessful")]
+    public bool Success { get; set; }
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+}
