371dc6c96eaecb37bf369c68f81c16beeea1887e
TPGPL <TPGPL@users.noreply.github.com>
2023-12-02 20:40:52 +0100

    Add required models

---
+++ Models/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace PamiwMauiApp.Models
+{
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+        public bool Authenticated { get; set; }
+    }
+}
