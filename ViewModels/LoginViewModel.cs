bc7bb563d9303939242965ae9c7b8dca42be9090
TPGPL <TPGPL@users.noreply.github.com>
2023-12-02 21:13:48 +0100

    Implement registration/login

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using pamiw_pwa.Services;
+using PamiwMauiApp.Components;
 using PamiwMauiApp.Models;
 using PamiwMauiApp.Views;
 
@@ -7,13 +9,37 @@ namespace PamiwMauiApp.ViewModels
 {
     public partial class LoginViewModel : ObservableObject
     {
+        private readonly MauiMessageDialogService _messageDialogService;
+        private readonly IAuthService _authService;
+
         [ObservableProperty]
-        UserLogin user;
+        UserLogin user = new UserLogin();
+
+        public LoginViewModel(MauiMessageDialogService messageDialogService, IAuthService authService)
+        {
+            _messageDialogService = messageDialogService;
+            _authService = authService;
+        }
 
         [RelayCommand]
-        public void Login()
+        public async Task Login()
         {
-            Shell.Current.FlyoutBehavior = FlyoutBehavior.Flyout;
+            var userData = new UserLogin()
+            {
+                Username = User.Username,
+                Password = User.Password,
+                ConfirmPassword = User.ConfirmPassword
+            };
+
+            var response = await _authService.LoginAsync(userData);
+
+            if (!response.Success)
+            {
+                _messageDialogService.ShowMessage(response.Message ?? "Failed to login.");
+                return;
+            }
+
+            await Shell.Current.GoToAsync($"//{nameof(MainPage)}", true);
         }
 
         [RelayCommand]
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using pamiw_pwa.Services;
+using PamiwMauiApp.Components;
 using PamiwMauiApp.Models;
 using System;
 using System.Collections.Generic;
@@ -10,7 +13,39 @@ namespace PamiwMauiApp.ViewModels
 {
     public partial class RegisterViewModel : ObservableObject
     {
+        private readonly MauiMessageDialogService _messageDialogService;
+        private readonly IAuthService _authService;
+
         [ObservableProperty]
-        private UserRegister user;
+        UserRegister user = new UserRegister();
+
+        public RegisterViewModel(MauiMessageDialogService messageDialogService, IAuthService authService)
+        {
+            _messageDialogService = messageDialogService;
+            _authService = authService;
+        }
+
+        [RelayCommand]
+        public async Task Register()
+        {
+            var newUser = new UserRegister()
+            {
+                Username = User.Username,
+                Email = User.Email,
+                Password = User.Password
+            };
+
+            var response = await _authService.RegisterAsync(newUser);
+
+            if (response.Message is not null && response.Message.Any())
+            {
+                _messageDialogService.ShowMessage(response.Message);
+            }
+
+            if (response.Success)
+            {
+                await Shell.Current.GoToAsync("../", true);
+            }
+        }
     }
 }
