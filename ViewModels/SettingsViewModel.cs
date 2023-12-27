def241d8eb2e8e473bc762683c508ba3a6de6839
TPGPL <TPGPL@users.noreply.github.com>
2023-12-27 22:23:41 +0100

    Implement language switching

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using PamiwMauiApp.Resources.Strings;
 using System.Collections.ObjectModel;
+using System.Globalization;
 
 namespace PamiwMauiApp.ViewModels;
 
@@ -10,22 +12,32 @@ public partial class SettingsViewModel : BaseViewModel
     AppTheme selectedTheme = Application.Current.RequestedTheme;
 
     [ObservableProperty]
-    string selectedLanguage = "Polish";
+    string selectedLanguage = AppResources.Culture.NativeName;
 
     [ObservableProperty]
-    ObservableCollection<string> languages = ["Polish", "English"];
+    ObservableCollection<string> languages = [CultureInfo.GetCultureInfo("pl-PL").NativeName, CultureInfo.GetCultureInfo("en-US").NativeName];
 
     [ObservableProperty]
     ObservableCollection<AppTheme> themes = [AppTheme.Dark, AppTheme.Light];
 
-    public SettingsViewModel()
+    [ObservableProperty]
+    LocalizationResourceManager localizationResourceManager;
+
+    public SettingsViewModel(LocalizationResourceManager localizationResourceManager)
     {
-        Title = "Settings";
+        this.localizationResourceManager = localizationResourceManager;
     }
 
     [RelayCommand]
     public void SaveChanges()
     {
         Application.Current.UserAppTheme = SelectedTheme;
+
+        LocalizationResourceManager.SetCulture(SelectedLanguage switch
+        {
+            "polski (Polska)" => CultureInfo.GetCultureInfo("pl-PL"),
+            "English (United States)" => CultureInfo.GetCultureInfo("en-US"),
+            _ => CultureInfo.GetCultureInfo("en-US")
+        });
     }
 }
